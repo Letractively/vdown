e5a47a3c14f06d633e4e01902d3cf82e4f6af8a7
pyhost <pyhost@febc6c02-d537-0410-890a-557ae060572e>
2007-08-25 13:25:12 +0000

    commit some more files to restructure code

---
+++ trunk/vdown2_handler.py
@@ -0,0 +1,11 @@
+import handler
+
+class GUI_Handler(handler.Handler):
+    def __init__(self):
+        handler.Handler.__init__(self)
+
+
+if __name__ == "__main__":
+    print """
+do not run this file directly
+    """
