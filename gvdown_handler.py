424b5dc4e09b5308d185f6ef7244beff6e9875a5
kopfgeldjaeger <kopfgeldjaeger@febc6c02-d537-0410-890a-557ae060572e>
2007-08-25 16:15:56 +0000

    restored most signals in gvdown_handler.py, name cleanup

--- gvdown_handler.py
+++ gvdown_handler.py
@@ -1,22 +1,47 @@
 import gtk.glade
 import handler
+import os
 
 class GUI_Handler(handler.Handler):
     def __init__(self):
         handler.Handler.__init__(self)
         self.__file = None
 
-    def on_menu_file_open_activate(self, mi):
-        fc = self.get_widget(mi, "filechooserdialog")
-        fc.show()
+    def menu_file_open_clicked(self, widget, event):
+        filechooser = self.get_widget(widget, "filechooserdialog")
+        filechooser.set_current_folder(os.path.expanduser("~"))
+        filechooser.show()
 
-    def on_button_open_file_clicked(self, button):
-        fc = self.get_widget(button, "filechooserdialog")
-        self.__file = fc.get_filename()
-        print self.__file
-        fc.hide() 
-    
-    
+    def menu_help_info_clicked(self, widget, event):
+        aboutdialog = self.get_widget(widget, "aboutdialog")
+        aboutdialog.show()
+
+    def on_fc_button_open_file_clicked(self, widget): # Chose file in filechooserdialog and pressed "Open"
+        filechooser = self.get_widget(widget, "filechooserdialog")
+        self.chosen_file = filechooser.get_filename()
+        print "%s selected." % self.chosen_file
+        filechooser.hide()
+
+    def on_fc_button_cancel_clicked(self, widget): # Clicked "Cancel" in filechooser
+        filechooser = self.get_widget(widget, "filechooserdialog")
+        filechooser.hide()
+
+    def closedSomehow(self, widget, event = None):
+        gtk.main_quit()
+
+    def on_aboutdialog_delete(self, widget, event):
+        aboutdialog = self.get_widget(widget, "aboutdialog")
+        aboutdialog.hide()
+
+    def on_filechooserdialog_delete(self, widget):
+        filechooser = self.get_widget(widget, "filechooserdialog")
+        filechooser.hide()
+
+    def mainDownload_button_clicked(self, widget):
+        entry_url = self.get_widget(widget, "entry_url")
+        entry_content = entry_url.get_text()
+        print 'You entered: "%s"' % entry_content
+
 if __name__ == "__main__":
     print """
 do not run this file directly
