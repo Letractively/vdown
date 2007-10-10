e93a88377e3363fd80f1075c3e6ce4654faf8209
kopfgeldjaeger <kopfgeldjaeger@febc6c02-d537-0410-890a-557ae060572e>
2007-10-10 18:14:55 +0000

    implemented download function gui, still work needed

--- trunk/gui.py
+++ trunk/gui.py
@@ -16,7 +16,8 @@ except:
 	sys.exit(1)
 
 from time import sleep
-import main
+from main import get_data
+from download import fdownload
 
 gladefile = "gvdown.glade"
 
@@ -26,15 +27,16 @@ class gui:
 		dic = {"closedSomehow" : self.closedSomehow,
 			"download_single" : self.download_single}
 		self.wTree.signal_autoconnect(dic)
+		self.wTree.get_widget("dprogressbar").set_text("Nothing to do")
 
 	def closedSomehow(self, widget, event = None):
 		gtk.main_quit()
 
 	def download_single(self, widget, event = None):
 		entry_url = self.wTree.get_widget("entry_url")
+		pb = self.wTree.get_widget("dprogressbar")
 		url = entry_url.get_text()
 		if url == "":
-			sleep(3)
 			dialog = gtk.MessageDialog(type=gtk.MESSAGE_ERROR,
 							message_format = "No URL specified!",
 							buttons=gtk.BUTTONS_OK)
@@ -42,10 +44,26 @@ class gui:
 			dialog.run()
 			dialog.destroy()
 		else:
-			print "URL: %s" % (url)
+			pb.set_text("Fetching video information...")
+			print "Trying to download %s" % (url)
+			data = get_data(url)
+			print "Saving file as \"%s\"..." % (data[2])
+			down = fdownload(data[0], data[2])
+			down.start()
+			pb.set_text("Downloading video...")
+			progress = down.downloaded()/100
+			while down.get_filesize() == 0:
+				gtk.main_iteration_do(False)
+			gtk.main_iteration_do(True)
+			filesize = down.get_filesize()
+			print "Filesize: %s KB" % (filesize)
+			while progress < 1:
+				gtk.main_iteration_do(False)
+				pb.set_fraction(progress)
+				progress = down.downloaded()/100
+			gtk.main_iteration_do(True)
+			pb.set_text("Download finished.")
 		
-		
-
 if __name__ == "__main__":
 	try:
 		fileread = open(gladefile)
