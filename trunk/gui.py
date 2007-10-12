0ba3fc4978a428980cdfc1c375c7d578d136bb3c
kopfgeldjaeger <kopfgeldjaeger@febc6c02-d537-0410-890a-557ae060572e>
2007-10-12 21:19:41 +0000

    improved gui (doesnt hang anymore), get_data(url) is now threaded

--- trunk/gui.py
+++ trunk/gui.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-import sys
+#***************************************************************************\
+#*   Copyright (C) 2007 by Nicolai Spohrer,                                *
+#*   nicolai@xeve.de                                                       *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU General Public License as published by  *
+#*   the Free Software Foundation; either version 3 of the License, or     *
+#*   (at your option) any later version.                                   *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU General Public License for more details.                          *
+#*                                                                         *
+#*   You should have received a copy of the GNU General Public License     *
+#*   along with this program; if not, see                                  *
+#*   <http://www.gnu.org/licenses/gpl.html> or write to the                *
+#*   Free Software Foundation, Inc.,                                       *
+#*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+#***************************************************************************
+# Filename   : mgui.py                                                     *
+# Description: Downloads videos from video sharing websites like YouTube,  *
+#       Myspace Video, Google Video, Clipfish and so on.                   *
+# This application uses <videograb.de>                                     *
+# The video will be saved as FLV file.                                     *
+#***************************************************************************/
+
+import os, sys
 
 try:
 	import pygtk
@@ -17,7 +45,7 @@ except:
 
 from time import sleep
 from main import get_data
-from download import fdownload
+from main import fdownload
 
 gladefile = "gvdown.glade"
 
@@ -25,7 +53,10 @@ class gui:
 	def __init__(self):
 		self.wTree = gtk.glade.XML(gladefile)
 		dic = {"closedSomehow" : self.closedSomehow,
-			"download_single" : self.download_single}
+			"download_single" : self.download_single,
+			"menu_help_info_clicked" : self.menu_help_info_clicked,
+			"on_aboutdialog_delete" : self.on_aboutdialog_delete,
+			"menu_file_open_clicked" : self.menu_file_open_clicked}
 		self.wTree.signal_autoconnect(dic)
 		self.wTree.get_widget("dprogressbar").set_text("Nothing to do")
 
@@ -41,29 +72,59 @@ class gui:
 		if url == "":
 			pb.set_text("No URL specified")
 		else:
+			print "----"
 			print "Trying to download %s" % (url)
-			data = get_data(url)
-			print "Saving file as \"%s\"..." % (data[2])
-			down = fdownload(data[0], data[2])
-			down.start()
 			pb.set_fraction(0)
-			pb.set_text("Downloading video...")
-			progress = down.downloaded()/100
-			while down.get_filesize() == 0:
+			data = get_data(url)
+			data.start()
+			while data.status == -1:
 				gtk.main_iteration_do(False)
 				sleep(0.01)
 			gtk.main_iteration_do(True)
-			filesize = down.get_filesize()
-			print "Filesize: %s KB" % (filesize)
-			while progress < 1:
-				gtk.main_iteration_do(False)
-				sleep(0.01)
-				pb.set_fraction(progress)
+			if data.status == 0:
+				print "Saving file as \"%s\"..." % (data.data[2])
+				down = fdownload(data.data[0], data.data[2])
+				down.start()
+				pb.set_fraction(0)
+				pb.set_text("Downloading video...")
 				progress = down.downloaded()/100
-			pb.set_fraction(1)
-			gtk.main_iteration_do(True)
-			pb.set_text("Download finished.")
-		
+				while down.get_filesize() == 0:
+					gtk.main_iteration_do(False)
+					sleep(0.01)
+				gtk.main_iteration_do(True)
+				filesize = down.get_filesize()
+				print "Filesize: %s KB" % (filesize)
+				while progress < 1:
+					gtk.main_iteration_do(False)
+					sleep(0.01)
+					pb.set_fraction(progress)
+					progress = down.downloaded()/100
+				gtk.main_iteration_do(True)				
+				pb.set_fraction(1)
+				pb.set_text("Download finished.")
+			else:
+				pb.set_text("Wrong URL / unsupported video portal")
+			print "----"
+
+##
+	def menu_help_info_clicked(self, widget, event = None):
+		aboutdialog = self.wTree.get_widget("aboutdialog")
+		aboutdialog.show()
+
+	def on_aboutdialog_delete(self, widget, event):
+		aboutdialog = self.wTree.get_widget("aboutdialog")
+		aboutdialog.hide()
+		return True
+##
+
+	def menu_file_open_clicked(self, widget):
+		fc = self.wTree.get_widget("filechooserdialog")
+		fc.set_current_folder(os.path.expanduser("~"))
+		fc.show()
+
+	def fc_open_file_clicked(self, widget): # Clicked on button 'open' in filechooserdialog
+		print "doesnt do anything yet"
+
 if __name__ == "__main__":
 	try:
 		fileread = open(gladefile)
--- trunk/main.py
+++ trunk/main.py
@@ -25,39 +25,74 @@
 # Description: Downloads videos from video sharing websites like YouTube,  *
 #       Myspace Video, Google Video, Clipfish and so on.                   *
 # This application uses <videograb.de>                                     *
-# The video will be saved as FLV file in your home directory.              *
+# The video will be saved as FLV file.                                     *
 #***************************************************************************/
 
-import sys, httplib, re
+import sys, httplib, re, urllib2, threading, time
 
-def get_data(url):
-	SITE="www.videograb.de"
-	FILENAME="/cgi-bin/video.cgi?url="+url
 
-	con_vg=httplib.HTTPConnection(SITE)
-	con_vg.request("GET", FILENAME)
-	con_vg_info = con_vg.getresponse()
-	con_vg_data=con_vg_info.read()
+class fdownload(threading.Thread): # not used in this file, but by the other interfaces
+	def __init__(self, url, file):
+		self.file = open(file,'wb')
+		self.url = url
+		threading.Thread.__init__(self)
+		self.arived_len = 0
+		self.content_len = 100
+		self.downloaded = lambda: 100.0*self.arived_len/int(self.content_len) 
+		self.filesize=int(self.content_len)/1024 # in KB
 
-	try:
-		WANTEDLINE=[ l for l in con_vg_data.splitlines() if ">Download von " in l][0] # echo "rabfoo \nfoobar" | grep bar
-	except IndexError:
-		print "Could not grep the wanted line! Wrong URL or unsupported video portal!"
-		raise Exception, "Could not grep the wanted line!"
-	else:
-		WANTEDLINK=re.sub(">Download$", "", re.sub("\"", "", re.sub("href\=", "", re.split(" ", WANTEDLINE)[1])))
-		WANTEDNAME=re.sub("<br>$", "", re.split("</a>: ", WANTEDLINE)[1])
-		VIDEO_FILENAME=re.sub("(?i).flv.flv", ".flv", WANTEDNAME+".flv")
-		return [WANTEDLINK, WANTEDNAME, VIDEO_FILENAME]
+	def run(self):
+		req = urllib2.Request(self.url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)')
+		con = urllib2.urlopen(req)
+		self.content_len = con.info()['Content-length']
+		arived_len = 0
+		for chunk in con:
+			self.arived_len += len(chunk)
+			self.file.write(chunk)
+		self.file.close()
+	def get_filesize(self):
+		return int(self.content_len)/1024
+
+class get_data(threading.Thread):
+	def __init__(self, url):
+		threading.Thread.__init__(self)
+		self.status = -1
+		self.url = url
+		self.data = [None, None, None]
+	def run(self):
+		SITE="www.videograb.de"
+		FILENAME="/cgi-bin/video.cgi?url="+self.url
+	
+		con_vg=httplib.HTTPConnection(SITE)
+		con_vg.request("GET", FILENAME)
+		con_vg_info = con_vg.getresponse()
+		con_vg_data=con_vg_info.read()
+	
+		try:
+			WANTEDLINE=[ l for l in con_vg_data.splitlines() if ">Download von " in l][0] # echo "rabfoo \nfoobar" | grep bar
+		except IndexError:
+			self.status = 1
+		else:
+			WANTEDLINK=re.sub(">Download$", "", re.sub("\"", "", re.sub("href\=", "", re.split(" ", WANTEDLINE)[1])))
+			WANTEDNAME=re.sub("<br>$", "", re.split("</a>: ", WANTEDLINE)[1])
+			VIDEO_FILENAME=re.sub("(?i).flv.flv", ".flv", WANTEDNAME+".flv")
+			self.status = 0
+			self.data = [WANTEDLINK, WANTEDNAME, VIDEO_FILENAME]
 
 if __name__ == "__main__":
 	for i in sys.argv:
 		if i != sys.argv[0]:
 			print "----"
 			print "URL           : "+i
-			try:
-				data = get_data(i)
-				print "Download link : "+data[0]
-				print "Video name: "+data[1]
-			except Exception:
-				print "Could not get infos, trying next URL (if any)..."
+
+			data = get_data(i)
+			data.start()
+			while data.status == -1:
+				time.sleep(0.02)
+			if data.status == 0:
+				print "Download link : "+data.data[0]
+				print "Video name    : "+data.data[1]
+			else: 
+				print "Could not fetch the wanted line. Wrong URL or unsupported video portal!"
+			print "----"
