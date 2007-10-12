5e554c659a9dd85f8f1bf8b79ecd56278c146b41
kopfgeldjaeger <kopfgeldjaeger@febc6c02-d537-0410-890a-557ae060572e>
2007-10-12 21:19:41 +0000

    improved gui (doesnt hang anymore), get_data(url) is now threaded

--- cli.py
+++ cli.py
@@ -25,7 +25,7 @@
 # Description: Downloads videos from video sharing websites like YouTube,  *
 #       Myspace Video, Google Video, Clipfish and so on.                   *
 # This application uses <videograb.de>                                     *
-# The video will be saved as FLV file in your home directory.              *
+# The video will be saved as FLV file.                                     *
 #***************************************************************************/
 
 from main import get_data
@@ -36,46 +36,50 @@ from time import sleep
 
 for i in sys.argv:
 	if i != sys.argv[0]:
-		print "---"
+		print "----"
 		print "Trying to download the video..."
 		print "URL: "+i
 		try: 
 			data = get_data(i)
-			print "Saving file as \"%s\"..." % (data[2])
-			down = download.fdownload(data[0], data[2])
-			down.start()
-			progress = down.downloaded()
-			last_progress = progress
-			last_ETA = 0.00
-			sleep(1) # give "it" some time to get the filesize, otherwise it'd be just '100'
-			filesize = down.get_filesize()
-			while filesize == 0: # if it was not enough
-				sleep(1)
-				filesize = down.get_filesize()
-			print "Filesize: ",filesize,"KB"
-			while True:
-				progress_dif = progress-last_progress
-				kb_per_sec = filesize*(progress_dif/100)
-				downloaded_kb = (progress/100)*filesize
-				left_kb = filesize-downloaded_kb
-				kb_per_sec_asfloat = "%.2f" % (float(kb_per_sec))
-				if kb_per_sec < 1:
-					if last_ETA == 0.00:
-						ETA = "?"
-				else:
-					ETA = "%.2f" % (left_kb/kb_per_sec) # may be inexact!
-					last_ETA = ETA # if kb_per_sec is 0 and something was already downloaded, print the last ETA
-				sys.stdout.write("\r%.2f \033[6G percent downloaded | %s \033[35G KB/s | ETA: %s \033[55Gseconds" % (float(progress), str(kb_per_sec_asfloat).rjust(7), ETA)) # makes it look more static
-				sys.stdout.flush()
-				last_progress = progress
-				sleep(1)
+			data.start()
+			while data.status == -1:
+				time.sleep(0.02)
+			if data.status == 0:
+				print "Saving file as \"%s\"..." % (data.data[2])
+				down = download.fdownload(data.data[0], data.data[2])
+				down.start()
 				progress = down.downloaded()
-				if(progress == 100.0):
-					sys.stdout.write("\rDownload finished! Trying next (if any)...                      \n")
-					break
+				last_progress = progress
+				last_ETA = 0.00
+				sleep(1) # give "it" some time to get the filesize, otherwise it'd be just '100'
+				filesize = down.get_filesize()
+				while filesize == 0: # if it was not enough
+					sleep(1)
+					filesize = down.get_filesize()
+				print "Filesize: ",filesize,"KB"
+				while True:
+					progress_dif = progress-last_progress
+					kb_per_sec = filesize*(progress_dif/100)
+					downloaded_kb = (progress/100)*filesize
+					left_kb = filesize-downloaded_kb
+					kb_per_sec_asfloat = "%.2f" % (float(kb_per_sec))
+					if kb_per_sec < 1:
+						if last_ETA == 0.00:
+							ETA = "?"
+					else:
+						ETA = "%.2f" % (left_kb/kb_per_sec) # may be inexact!
+						last_ETA = ETA # if kb_per_sec is 0 and something was already downloaded, print the last ETA
+					sys.stdout.write("\r%.2f \033[6G percent downloaded | %s \033[35G KB/s | ETA: %s \033[55Gseconds" % (float(progress), str(kb_per_sec_asfloat).rjust(7), ETA)) # makes it look more static
+					sys.stdout.flush()
+					last_progress = progress
+					sleep(1)
+					progress = down.downloaded()
+					if(progress == 100.0):
+						sys.stdout.write("\rDownload finished! Trying next (if any)...                      \n")
+						break
+			else:
+				print "Could not fetch the wanted line. Wrong URL or unsupported video portal!"
 		except KeyboardInterrupt:
 			print "\nKilled by STRG+C, quitting..."
 			sys.exit(1)
-#		except Exception:
-#			print "\nCould not find out video download link! Trying next (if any)..."
-		print "---"
+		print "----"
