f3c3d5f9ce9226e954bcdb65a8d2ad4506a7d8fd
kopfgeldjaeger <kopfgeldjaeger@febc6c02-d537-0410-890a-557ae060572e>
2007-10-16 20:18:16 +0000

    code cleanup, finished gettext compatibiliy, added german translation :)

--- cli.py
+++ cli.py
@@ -32,120 +32,95 @@ from main import fdownload
 from main import convert as fconvert
 from main import get_data
 from main import folder_is_writable
+from main import configuration
 import os, sys, time
 import re
 from time import sleep
-import ConfigParser
-from gettext import gettext as _
+import gettext
+
+# <Tranlations stuff>
+
+gettext.install("gvdown", "./po", unicode=True)
+
+# </Translation stuff>
 
 try:
-	if os.path.isdir(sys.argv[1]):
-		save_videos_in = sys.argv[1]
-	else:
-		save_videos_in = "."
-
+    if os.path.isdir(sys.argv[1]):
+        save_videos_in = sys.argv[1]
+    else:
+        save_videos_in = "."
 except IndexError:
-	print _("Please specify at least one parameter.")
-	print _("Examples: ")
-	print sys.argv[0]+_(" /home/testuser/videos URL1 [URL2 ...]")
-	print _("or")
-	print sys.argv[0]+_(" URL1 [URL2 ...]")
-	sys.exit(1)
+    print _("Please specify at least one parameter.")
+    print _("Examples: ")
+    print sys.argv[0]+_(" /home/testuser/videos URL1 [URL2 ...]")
+    print _("or")
+    print sys.argv[0]+_(" URL1 [URL2 ...]")
+    sys.exit(1)
 
 if not folder_is_writable(save_videos_in):
-	print _("Can't write to this directory! Change to another.")
-	sys.exit(1)
+    print _("Can't write to this directory! Change to another.")
+    sys.exit(1)
 
-homedir = os.path.expanduser("~")
-configfilename = homedir+"/.gvdownrc"
-config = ConfigParser.RawConfigParser()
-
-if not os.path.isfile(configfilename):
-	config.add_section("general")
-	config.set("general", "convert", "no")
-	config.set("general", "convertcmd", "convertsth --input %i --output %o")
-	config.set("general", "convert_filename_extension", ".avi")
-	config.set("general", "delete_source_file_after_converting", "no")
-	write_config()
-
-config.readfp(open(configfilename))
-try:
-	convert = config.getboolean("general", "convert")
-	convertcmd = config.get("general", "convertcmd")
-	convert_filename_extension = config.get("general", "convert_filename_extension")
-	deletesourcefile = config.getboolean("general", "delete_source_file_after_converting")
-except ConfigParser.NoSectionError: # if not all settings are set, set all settings to default ;)
-	config.add_section("general")
-	config.set("general", "convert", "no")
-	config.set("general", "convertcmd", "convertsth --input %i --output %o")
-	config.set("general", "convert_filename_extension", ".avi")
-	config.set("general", "delete_source_file_after_converting", "no")
-	write_config()
-	save_videos_in = config.get("general", "save_videos_in")
-	convert = config.getboolean("general", "convert")
-	convertcmd = config.get("general", "convertcmd")
-
-	def write_config(self):
-		f = file(configfilename, "w")
-		config.write(f)
-		f.close()
+config = configuration()
+config.readconfig()
 
 for i in sys.argv:
-	if i != sys.argv[0] and i != save_videos_in:
-		print "----"
-		print _("Trying to download the video...")
-		print _("URL: ")+i
-		try: 
-			data = get_data(i)
-			data.start()
-			while data.status == -1:
-				time.sleep(0.02)
-			if data.status == 0:
-				print _("Saving file as \"%s\"...") % (data.data[2])
-				down = fdownload(data.data[0], save_videos_in+"/"+data.data[2])
-				down.start()
-				progress = down.downloaded()
-				last_progress = progress
-				last_ETA = 0.00
-				sleep(1) # give "it" some time to get the filesize, otherwise it'd be just '100'
-				filesize = down.get_filesize()
-				while filesize == 0: # if it was not enough
-					sleep(1)
-					filesize = down.get_filesize()
-				print _("Filesize: "),filesize,"KB"
-				while True:
-					progress_dif = progress-last_progress
-					kb_per_sec = filesize*(progress_dif/100)
-					downloaded_kb = (progress/100)*filesize
-					left_kb = filesize-downloaded_kb
-					kb_per_sec_asfloat = "%.2f" % (float(kb_per_sec))
-					if kb_per_sec < 1:
-						if last_ETA == 0.00:
-							ETA = "?"
-					else:
-						ETA = "%.2f" % (left_kb/kb_per_sec) # may be inexact!
-						last_ETA = ETA # if kb_per_sec is 0 and something was already downloaded, print the last ETA
-					sys.stdout.write(_("\r%.2f \033[6G percent downloaded | %s \033[35G KB/s | ETA: %s \033[55Gseconds") % (float(progress), str(kb_per_sec_asfloat).rjust(7), ETA)) # makes it look more static
-					sys.stdout.flush()
-					last_progress = progress
-					sleep(1)
-					progress = down.downloaded()
-					if(progress == 100.0):
-						sys.stdout.write(_("\rDownload finished...                                                      \n"))
-						if convert:
-							print _("Converting file...")
-							output = fconvert(save_videos_in+"/"+data.data[2], convert_filename_extension, convertcmd)
-							output.start()
-							while output.status == -1:
-								sleep(0.2)
-							print _("Converted file.")
-							if deletesourcefile:
-								os.remove(save_videos_in+"/"+data.data[2])
-								print _("Deleted input (.flv) file")
-						break
-			else:
-				print _("Could not fetch the wanted line. Wrong URL or unsupported video portal! But better try again.")
-		except KeyboardInterrupt:
-			print _("\nKilled by STRG+C, quitting...")
-			sys.exit(1)
-		print "----"
+    if i != sys.argv[0] and i != save_videos_in:
+        print "----"
+        print _("Trying to download the video...")
+        print _("URL: %(url)s") % {"url" : i}
+        try:
+            data = get_data(i)
+            data.start()
+            while data.status == -1:
+                time.sleep(0.02)
+            if data.status == 0:
+                print _('Saving file as "%(file)s"...') % {"file" : data.data[2]}
+                down = fdownload(data.data[0], save_videos_in+"/"+data.data[2])
+                down.start()
+                progress = down.downloaded()
+                last_progress = progress
+                last_ETA = 0.00
+                sleep(1) # give "it" some time to get the filesize, otherwise it'd be just '100'
+                filesize = down.get_filesize()
+                while filesize == 0: # if it was not enough
+                    sleep(1)
+                    filesize = down.get_filesize()
+                print _("Filesize: %(filesize)s KB") % {"filesize" : filesize}
+                while True:
+                    progress_dif = progress-last_progress
+                    kb_per_sec = filesize*(progress_dif/100)
+                    downloaded_kb = (progress/100)*filesize
+                    left_kb = filesize-downloaded_kb
+                    kb_per_sec_asfloat = "%.2f" % (float(kb_per_sec))
+                    if kb_per_sec < 1:
+                        if last_ETA == 0.00:
+                            ETA = "?"
+                    else:
+                        ETA = "%.2f" % (left_kb/kb_per_sec) # may be inexact!
+                        last_ETA = ETA # if kb_per_sec is 0 and something was already downloaded, print the last ETA
+                    seconds_string = _("seconds") # gettext + this line under me...
+                    sys.stdout.write("\r%.2f \033[6G %% | %s \033[18G KB/s | ETA: %s \033[38G%s" % (float(progress), str(kb_per_sec_asfloat).rjust(7), ETA, seconds_string)) # makes it look more static
+                    sys.stdout.flush()
+                    last_progress = progress
+                    sleep(1)
+                    progress = down.downloaded()
+                    if(progress == 100.0):
+                        sys.stdout.write(_("\rDownload finished...                                                      \n"))
+                        if config.getboolean("general", "convert"):
+                            print _("Converting file...")
+                            output = fconvert(save_videos_in+"/"+data.data[2], config.get("general", "convert_filename_extension"), config.get("general", "convertcmd"))
+                            output.start()
+                            while output.status == -1:
+                                sleep(0.2)
+                            print _("Converted file.")
+                            if config.getboolean("general", "delete_source_file_after_converting"):
+                                os.remove(save_videos_in+"/"+data.data[2])
+                                print _("Deleted input (.flv) file")
+                        break
+            else:
+                print _("Could not fetch the wanted line. Wrong URL or unsupported video portal! But better try again.")
+        except KeyboardInterrupt:
+            print _("\nKilled by CTRL+C, quitting...")
+            sys.exit(1)
+        print "----"
--- main.py
+++ main.py
@@ -28,108 +28,166 @@
 # The video will be saved as FLV file.                                     *
 #***************************************************************************/
 
-import os, sys, httplib, re, urllib2, threading, time, subprocess
+import os
+import sys
+import httplib
+import re
+import urllib2
+import threading
+import time
+import subprocess
+import ConfigParser
+import gettext
 
+# <Tranlations stuff>
+
+gettext.install("gvdown", "./po", unicode=True)
+
+# </Translation stuff>
 
 class fdownload(threading.Thread): # not used in this file, but by the other interfaces
-	def __init__(self, url, file):
-		self.file = open(file,'wb')
-		self.url = url
-		threading.Thread.__init__(self)
-		self.arived_len = 0
-		self.content_len = 100
-		self.downloaded = lambda: 100.0*self.arived_len/int(self.content_len) 
-		self.filesize=int(self.content_len)/1024 # in KB
+    def __init__(self, url, file):
+        self.file = open(file,'wb')
+        self.url = url
+        threading.Thread.__init__(self)
+        self.arived_len = 0
+        self.content_len = 100
+        self.downloaded = lambda: 100.0*self.arived_len/int(self.content_len)
+        self.filesize=int(self.content_len)/1024 # in KB
 
-	def run(self):
-		req = urllib2.Request(self.url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)')
-		con = urllib2.urlopen(req)
-		self.content_len = con.info()['Content-length']
-		arived_len = 0
-		for chunk in con:
-			self.arived_len += len(chunk)
-			self.file.write(chunk)
-		self.file.close()
-	def get_filesize(self):
-		return int(self.content_len)/1024
+    def run(self):
+        req = urllib2.Request(self.url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20061201 Firefox/2.0.0.6 (Ubuntu-feisty)')
+        con = urllib2.urlopen(req)
+        self.content_len = con.info()['Content-length']
+        arived_len = 0
+        for chunk in con:
+            self.arived_len += len(chunk)
+            self.file.write(chunk)
+        self.file.close()
+    def get_filesize(self):
+        return int(self.content_len)/1024
 
 class convert(threading.Thread):
-	def __init__(self, input, filename_extension, command):
-		threading.Thread.__init__(self)
-		self.status = -1
-		self.input = input
-		self.filename_extension = filename_extension
-		self.command = command
-	def run(self):
-		self.input = re.sub(" ", "-spacechar-filename-", self.input)
-		self.output = re.sub(".flv$", self.filename_extension, self.input)
-		self.output = re.sub(" ", "-spacechar-filename-", self.output)
-		convert_command_i_replaced = re.sub("%i", self.input, self.command)
-		convert_command = re.sub("%o", self.output, convert_command_i_replaced)
-		final_cmd = []
-		for i in convert_command.split():
-			final_cmd.append(i.replace("-spacechar-filename-", " "))
-		output = subprocess.Popen(final_cmd, stdout=subprocess.PIPE).communicate()[0]
-		self.status = 0
-		
+    """
+    Convert video files with user-specified command
+    """
+    def __init__(self, input, filename_extension, command):
+        threading.Thread.__init__(self)
+        self.status = -1
+        self.input = input
+        self.filename_extension = filename_extension
+        self.command = command
+    def run(self):
+        self.output = re.sub(".flv$", self.filename_extension, self.input)
+        final_cmd = []
+        for i in self.command.split():
+            final_cmd.append(i.replace("%i", self.input).replace("%o", self.output))
+        output = subprocess.Popen(final_cmd, stdout=subprocess.PIPE).communicate()[0]
+        self.status = 0
+
 
 class get_data(threading.Thread):
-	def __init__(self, url):
-		threading.Thread.__init__(self)
-		self.status = -1
-		self.url = url
-		self.data = [None, None, None]
-	def run(self):
-		SITE="www.videograb.de"
-		FILENAME="/cgi-bin/video.cgi?url="+self.url
-	
-		con_vg=httplib.HTTPConnection(SITE)
-		con_vg.request("GET", FILENAME)
-		con_vg_info = con_vg.getresponse()
-		con_vg_data=con_vg_info.read()
-	
-		try:
-			WANTEDLINE=[ l for l in con_vg_data.splitlines() if ">Download von " in l][0] # echo "rabfoo \nfoobar" | grep bar
-		except IndexError:
-			self.status = 1
-		else:
-			WANTEDLINK=re.sub(">Download$", "", re.sub("\"", "", re.sub("href\=", "", re.split(" ", WANTEDLINE)[1])))
-			WANTEDNAME=re.sub("<br>$", "", re.split("</a>: ", WANTEDLINE)[1])
-			VIDEO_FILENAME=re.sub("(?i).flv.flv", ".flv", WANTEDNAME+".flv")
-			self.status = 0
-			self.data = [WANTEDLINK, WANTEDNAME, VIDEO_FILENAME]
+    """
+    Fetch data from videograb.de
+    """
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.status = -1
+        self.url = url
+        self.data = [None, None, None]
+    def run(self):
+        SITE="www.videograb.de"
+        FILENAME="/cgi-bin/video.cgi?url="+self.url
+
+        con_vg=httplib.HTTPConnection(SITE)
+        con_vg.request("GET", FILENAME)
+        con_vg_info = con_vg.getresponse()
+        con_vg_data=con_vg_info.read()
+
+        try:
+            WANTEDLINE=[ l for l in con_vg_data.splitlines() if ">Download von " in l][0] # echo "rabfoo \nfoobar" | grep bar
+        except IndexError:
+            self.status = 1
+        else:
+            WANTEDLINK=re.sub(">Download$", "", re.sub("\"", "", re.sub("href\=", "", re.split(" ", WANTEDLINE)[1])))
+            WANTEDNAME=re.sub("<br>$", "", re.split("</a>: ", WANTEDLINE)[1])
+            VIDEO_FILENAME=re.sub("(?i).flv.flv", ".flv", WANTEDNAME+".flv")
+            self.status = 0
+            self.data = [WANTEDLINK, WANTEDNAME, VIDEO_FILENAME]
 
 def folder_is_writable(dir):
-	if os.path.isfile(dir+"/vdown_test.testfile"): # do not delete the test file if it exists
-		EXISTS = True
-	else:
-		EXISTS = False
+    """
+    Check if we can write into a folder (creates a testfile there)
+    """
+    if os.path.isfile(dir+"/vdown_test.testfile"): # do not delete the test file if it exists
+        EXISTS = True
+    else:
+        EXISTS = False
 
-	try:
-		file = open(dir+"/vdown_test.testfile", "wb")
-		file.close()
-	except IOError:
-		return False
-	else:
-		if not EXISTS:
-			os.remove(dir+"/vdown_test.testfile")
-		return True
+    try:
+        file = open(dir+"/vdown_test.testfile", "wb")
+        file.close()
+    except IOError:
+        return False
+    else:
+        if not EXISTS:
+            os.remove(dir+"/vdown_test.testfile")
+        return True
+
+class configuration(ConfigParser.RawConfigParser):
+    """
+    Configuration class (based on RawConfigParser)
+    If an error occurs while reading the settings (e.g. ConfigParser.NoSectionError), all settings will be set to default.
+    """
+    def __init__(self):
+        ConfigParser.RawConfigParser.__init__(self)
+        self.configfilename = os.path.expanduser("~")+"/.gvdownrc"
+        if not os.path.isfile(self.configfilename):
+            print _("Config file %s not found. Creating one for you..." % (self.configfilename))
+            self.set_defaults()
+            self.write_config()
+
+    def readconfig(self):
+        self.readfp(open(self.configfilename))
+        try:
+            test = self.get("general", "save_videos_in")
+            test = self.getboolean("general", "convert")
+            test = self.get("general", "convertcmd")
+            test = self.get("general", "convert_filename_extension")
+            test = self.getboolean("general", "delete_source_file_after_converting")
+        except:
+            self.set_defaults()
+            self.write_config()
+
+    def set_defaults(self):
+        print _("Setting settings to default...")
+        if not self.has_section("general"):
+            self.add_section("general")
+        self.set("general", "save_videos_in", os.path.expanduser("~/downloads"))
+        self.set("general", "convert", "no")
+        self.set("general", "convertcmd", "convertsth --input %i --output %o")
+        self.set("general", "convert_filename_extension", ".avi")
+        self.set("general", "delete_source_file_after_converting", "no")
+
+    def write_config(self):
+        f = file(self.configfilename, "w")
+        self.write(f)
+        f.close()
 
 if __name__ == "__main__":
-	for i in sys.argv:
-		if i != sys.argv[0]:
-			print "----"
-			print "URL           : "+i
+    for i in sys.argv:
+        if i != sys.argv[0]:
+            print "----"
+            print "URL           : "+i
 
-			data = get_data(i)
-			data.start()
-			while data.status == -1:
-				time.sleep(0.02)
-			if data.status == 0:
-				print "Download link : "+data.data[0]
-				print "Video name    : "+data.data[1]
-			else: 
-				print "Could not fetch the wanted line. Wrong URL or unsupported video portal!"
-			print "----"
-
+            data = get_data(i)
+            data.start()
+            while data.status == -1:
+                time.sleep(0.02)
+            if data.status == 0:
+                print "Download link : "+data.data[0]
+                print "Video name    : "+data.data[1]
+            else:
+                print "Could not fetch the wanted line. Wrong URL or unsupported video portal!"
+            print "----"
