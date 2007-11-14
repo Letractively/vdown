7e94cd5614aae8a68e727dda58664d6ab17634a1
kopfgeldjaeger <kopfgeldjaeger@febc6c02-d537-0410-890a-557ae060572e>
2007-11-14 15:49:42 +0000

    added SAVE-TO option in cli, updated translation, added OptionParser

--- trunk/cli.py
+++ trunk/cli.py
@@ -38,6 +38,7 @@ import re
 from time import sleep
 import gettext
 from user import home as userhome
+from optparse import OptionParser
 
 # <Tranlations stuff>
 
@@ -45,89 +46,95 @@ gettext.install("gvdown", "po", unicode=True)
 
 # </Translation stuff>
 
-try:
-    if os.path.isdir(sys.argv[1]):
-        save_videos_in = sys.argv[1]
-    else:
-        save_videos_in = "."
-except IndexError:
-    print _("Please specify at least one parameter.")
-    print _("Examples: ")
-    print sys.argv[0]+_(" /home/testuser/videos URL1 [URL2 ...]")
-    print _("or")
-    print sys.argv[0]+_(" URL1 [URL2 ...]")
-    sys.exit(1)
+parser = OptionParser(usage=_("Usage: %prog [options] URL1 [URL2] [...]"))
 
-if not folder_is_writable(save_videos_in):
+parser.remove_option("-h")
+parser.add_option("-h", "--help", action="help", help=_("show this help message and exit"))
+
+parser.add_option("-d", "--destination", dest="destination",
+                  help=_("save videos in DEST"), metavar="DEST", default=".")
+
+parser.add_option("-s", "--save-as", dest="saveAs", help=_("save video file as FILE"), metavar="FILE", default=None)
+
+(options, args) = parser.parse_args()
+
+if not folder_is_writable(options.destination):
     print _("Can't write to this directory! Change to another.")
     sys.exit(1)
 
 config = configuration()
 config.readconfig()
 
-for i in sys.argv:
-    if i != sys.argv[0] and i != save_videos_in:
-        print "----"
-        print _("Trying to download the video...")
-        print _("URL: %(url)s") % {"url" : i}
-        try:
-            data = get_data(i)
-            data.start()
-            while data.status == -1:
-                time.sleep(0.02)
-            if data.status == 0:
-                saveAs = os.path.join(save_videos_in, data.data[2])
-                print _('Saving file as "%(file)s"...') % {"file" : saveAs}
-                down = fdownload(data.data[0], saveAs)
-                down.start()
-                progress = down.downloaded()
-                last_progress = progress
-                last_ETA = 0.00
-                putWinString = False
-                sleep(1) # give "it" some time to get the filesize, otherwise it'd be just '100'
-                filesize = down.get_filesize()
-                while filesize == 0: # if it was not enough
-                    sleep(1)
-                    filesize = down.get_filesize()
-                print _("Filesize: %(filesize)s KB") % {"filesize" : filesize}
-                while True:
-                    progress_dif = progress-last_progress
-                    kb_per_sec = filesize*(progress_dif/100)
-                    downloaded_kb = (progress/100)*filesize
-                    left_kb = filesize-downloaded_kb
-                    kb_per_sec_asfloat = "%.2f" % (float(kb_per_sec))
-                    if kb_per_sec < 1:
-                        if last_ETA == 0.00:
-                            ETA = "?"
-                    else:
-                        ETA = "%.2f" % (left_kb/kb_per_sec) # may be inexact!
-                        last_ETA = ETA # if kb_per_sec is 0 and something was already downloaded, print the last ETA
-                    seconds_string = _("seconds") # gettext + this one line under me...
-                    if sys.platform == "linux2":
-                        sys.stdout.write("\r%.2f \033[6G %% | %s \033[18G KB/s | ETA: %s \033[38G%s" % (float(progress), str(kb_per_sec_asfloat).rjust(7), ETA, seconds_string)) # makes it look more static
-                        sys.stdout.flush()
-                        last_progress = progress
-                        sleep(1)
-                    progress = down.downloaded()
-                    if sys.platform != "linux2" and putWinString == False:
-                        print _("Downloading...")
-                        putWinString = True
-                    if(progress == 100.0):
-                        sys.stdout.write(_("\rDownload finished...                                                      \n"))
-                        if config.getboolean("general", "convert") and data.data[3]:
-                            print _("Converting file...")
-                            output = fconvert(saveAs, config.get("general", "convert_filename_extension"), config.get("general", "convertcmd"))
-                            output.start()
-                            while output.status == -1:
-                                sleep(0.2)
-                            print _("Converted file.")
-                            if config.getboolean("general", "delete_source_file_after_converting"):
-                                os.remove(saveAs)
-                                print _("Deleted input (.flv) file")
-                        break
+if len(args) < 1:
+    print _("Specify at least one video.")
+    sys.exit(1)
+
+for i in args:
+
+    print "----"
+    print _("Trying to download the video...")
+    print _("URL: %(url)s") % {"url" : i}
+    try:
+        data = get_data(i)
+        data.start()
+        while data.status == -1:
+            sleep(0.02)
+        if data.status == 0:
+            if options.saveAs != None:
+                saveAs = options.saveAs
             else:
-                print _("Could not fetch the wanted line. Wrong URL or unsupported video portal! But better try again.")
-        except KeyboardInterrupt:
-            print _("\nKilled by CTRL+C, quitting...")
-            sys.exit(1)
-        print "----"
+                saveAs = os.path.join(options.destination, data.data[2])
+            print _('Saving file as "%(file)s"...') % {"file" : saveAs}
+            down = fdownload(data.data[0], saveAs)
+            down.start()
+            progress = down.downloaded()
+            last_progress = progress
+            last_ETA = 0.00
+            putWinString = False
+            sleep(1) # give "it" some time to get the filesize, otherwise it'd be just '100'
+            filesize = down.get_filesize()
+            while filesize == 0: # if it was not enough
+                sleep(1)
+                filesize = down.get_filesize()
+            print _("Filesize: %(filesize)s KB") % {"filesize" : filesize}
+            while True:
+                progress_dif = progress-last_progress
+                kb_per_sec = filesize*(progress_dif/100)
+                downloaded_kb = (progress/100)*filesize
+                left_kb = filesize-downloaded_kb
+                kb_per_sec_asfloat = "%.2f" % (float(kb_per_sec))
+                if kb_per_sec < 1:
+                    if last_ETA == 0.00:
+                        ETA = "?"
+                else:
+                    ETA = "%.2f" % (left_kb/kb_per_sec) # may be inexact!
+                    last_ETA = ETA # if kb_per_sec is 0 and something was already downloaded, print the last ETA
+                seconds_string = _("seconds") # gettext + this one line under me...
+                if sys.platform == "linux2":
+                    sys.stdout.write("\r%.2f \033[6G %% | %s \033[18G KB/s | ETA: %s \033[38G%s" % (float(progress), str(kb_per_sec_asfloat).rjust(7), ETA, seconds_string)) # makes it look more static
+                    sys.stdout.flush()
+                    last_progress = progress
+                    sleep(1)
+                progress = down.downloaded()
+                if sys.platform != "linux2" and putWinString == False:
+                    print _("Downloading...")
+                    putWinString = True
+                if(progress == 100.0):
+                    sys.stdout.write(_("\rDownload finished...                                                        \n"))
+                    if config.getboolean("general", "convert") and data.data[3]:
+                        print _("Converting file...")
+                        output = fconvert(saveAs, config.get("general", "convert_filename_extension"), config.get("general", "convertcmd"))
+                        output.start()
+                        while output.status == -1:
+                            sleep(0.2)
+                        print _("Converted file.")
+                        if config.getboolean("general", "delete_source_file_after_converting"):
+                            os.remove(saveAs)
+                            print _("Deleted input (.flv) file")
+                    break
+        else:
+            print _("Could not fetch the wanted line. Wrong URL or unsupported video portal! But better try again.")
+    except KeyboardInterrupt:
+        print _("\nKilled by CTRL+C, quitting...")
+        sys.exit(1)
+    print "----"
